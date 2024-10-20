5dba76c236cc1c6a2afd77297cacc0f8ec36cb36
Sourabh Choudhary <saurabh.bwn2001@gmail.com>
2024-10-21 03:46:15 +0530

    added contact list design

--- frontend/src/components/MainContainerComponents/ContactSection.jsx
+++ frontend/src/components/MainContainerComponents/ContactSection.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import contactListImage from '../../assets/BillGates.png'
 
 const ContactSection = () => {
   return (
-    <div className='contact-section'>ContactSection</div>
+    <div className='contact-section'>
+      <div className="search-bar">
+        <SearchIcon />
+        <input type="text" name="search" id="search" placeholder="Search" className='search-input-box' />
+      </div>
+      <div className="contact-list">
+        <h2 style={{ margin: '10px' }} >People</h2>
+        <div className="wrapper-parent">
+          <div className="wrapper">
+            <div className="contact-list-information">
+              <div className="contact-list-image">
+                <img src={contactListImage} alt="Contact list image" height={40} width={40} />
+              </div>
+              <div className="contact-information">
+                <div className="contact-name"><h3>Bill gates</h3></div>
+                <div className="contact-last-message"><span>Hi How are you Gates ?</span></div>
+              </div>
+            </div>
+            <div className="contact-timestamp-read">
+              <div className="timestamp"><p>Today, 3:00 am</p></div>
+              <div className="read"><p>✓✓</p></div>
+            </div>
+          </div>
+          <hr />
+        </div>
+        <div className="wrapper-parent">
+          <div className="wrapper">
+            <div className="contact-list-information">
+              <div className="contact-list-image">
+                <img src={contactListImage} alt="Contact list image" height={40} width={40} />
+              </div>
+              <div className="contact-information">
+                <div className="contact-name"><h3>Bill gates</h3></div>
+                <div className="contact-last-message"><span>Hi How are you Gates ?</span></div>
+              </div>
+            </div>
+            <div className="contact-timestamp-read">
+              <div className="timestamp"><p>Today, 3:00 am</p></div>
+              <div className="read"><p>✓✓</p></div>
+            </div>
+          </div>
+          <hr />
+        </div>
+      </div>
+    </div>
   )
 }
 
