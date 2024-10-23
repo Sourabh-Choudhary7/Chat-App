5d806b02fdb321931af60bc2f89fffd56f95c73f
Sourabh Choudhary <saurabh.bwn2001@gmail.com>
2024-10-24 04:08:01 +0530

    added login endpoints

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,7 +3,7 @@ import Cloudinary from 'cloudinary';
 import fs from 'fs/promises';
 import crypto from 'crypto';
 import AppError from '../utils/error.utils.js';
-import { generateJWTToken, hashPassword } from '../utils/auth.utils.js';
+import { comparePassword, generateJWTToken, hashPassword } from '../utils/auth.utils.js';
 
 const cookieOptions = {
     maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -14,7 +14,7 @@ const cookieOptions = {
 const register = async (req, res, next) => {
     try {
         const { userName, email, phone, password } = req.body;
-
+        console.log("phone: ", phone);
         if (!userName || !email || !phone || !password) {
             return next(new AppError('All fields are required', 400));
         }
@@ -95,7 +95,38 @@ const register = async (req, res, next) => {
 
 const login = async (req, res, next) => {
     try {
-        
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return next(new AppError('Email and password are required', 400));
+        }
+
+        const user = await User.findOne({ email }).select('+password');
+
+        if (!user) {
+            return next(new AppError('Invalid user id or user does not exist', 404));
+        }
+
+        // check if user's account is active or not
+        if (!user.account_active) {
+            return next(new AppError('Your Account is deactivated. Please contact support.', 403));
+        }
+
+        if (!(user && (await comparePassword(password, user.password)))) {
+            return next(new AppError('Email or Password do not match or user does not exist', 401));
+        }
+
+        // generate JWT token
+        const token = await generateJWTToken(user._id);
+        user.password = undefined;
+        res.cookie('token', token, cookieOptions);
+
+        res.status(200).json({
+            success: true,
+            message: 'User logged in successfully',
+            user,
+        });
+
     } catch (error) {
         return next(new AppError(error.message, 500));
     }
