bdf5059883b45c2f9b76a6da8ea3f9ceeea6ba3e
Sourabh Choudhary <saurabh.bwn2001@gmail.com>
2024-10-26 12:31:58 +0530

    fixed the chats access issue by other

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -6,16 +6,25 @@ import Message from '../models/message.model.js';
 
 const allMessages = async (req, res, next) => {
     try {
-        const { chatId } = req.body;
-        // const { id } = req.user;
-        // 1. Fetch the messages for the given chat ID
-        const messages = await Message.find({ chatId }).populate('sender', 'userName phone email');
-        if(!messages) {
+        const { chatId } = req.params;
+        console.log(chatId);
+        const { id } = req.user;
+        // 1. Check if the user have this chatId if not then don't able to see others message or chats
+        const chat = await Chat.findById(chatId);
+        if (!chat) {
+            return next(new AppError('Chat not found', 404));
+        }
+        if (!chat.members.includes(id)) {
+            return next(new AppError('You are not a member of this chat', 403));
+        }
+        // 2. Fetch the messages for the given chat ID
+        const messages = await Message.find({ chat: chatId }).populate('sender', 'userName phone email');
+        if (!messages) {
             return next(new AppError('No messages found for this chat', 404));
         }
         return res.status(200).json({
             success: true,
-            message:'all messages fetched successfully',
+            message: 'all messages fetched successfully',
             messages,
         });
     } catch (error) {
@@ -52,7 +61,7 @@ const sendMessage = async (req, res, next) => {
         // message.sender = await User.findById(message.sender).select('useName phone email');
         return res.status(201).json({
             success: true,
-            message:'Message sent successfully',
+            message: 'Message sent successfully',
             newMessage: message
         });
     } catch (error) {
