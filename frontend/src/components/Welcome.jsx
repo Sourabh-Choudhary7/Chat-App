d8120289b7a7940b3c0613d12177da2c4388f849
Sourabh Choudhary <saurabh.bwn2001@gmail.com>
2024-10-31 10:12:58 +0530

    added loggedIn username in welcome container

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import welcomeImg from '../assets/chats.png';
+import { useSelector } from 'react-redux';
 
 const Welcome = () => {
+  const userData = useSelector((state) => state?.auth?.userData);
+
   return (
     <div className="flex items-center justify-center gap-4 flex-[0.65] rounded-[25px] bg-gradient-to-r from-violet-500 to-fuchsia-500 p-2 shadow-2xl shadow-violet-700/40">
       <div className="w-1/2 px-4">
-      <h2 className='text-white text-3xl font-bold mb-2 drop-shadow-lg'>Hi Sourabh,👋</h2>
+        <h2 className='text-white text-3xl font-bold mb-2 drop-shadow-lg'>
+          Hi {userData?.userName.split(' ')[0].charAt(0).toUpperCase() + userData?.userName?.split(' ')[0].slice(1).toLowerCase()},👋
+        </h2>
         <h3 className="text-white text-xl font-medium mb-2 drop-shadow-lg">
           Welcome to ChatConnect!
         </h3>
